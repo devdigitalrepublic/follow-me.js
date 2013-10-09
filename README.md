3df52af05bb9b68124760e3bf467e9f9e9d32f1d
0x0049@gmail.com <0x0049@gmail.com>
2013-10-09 17:31:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+Div Follow
+===========
+
+Makes an element follow the screen, but only within the bounds of some element.
+
+Usage Example
+----
+
+This plugin assumes a two (or more) column setup. See the demo page if you need more help.
+
+```html
+<div id="container">
+    <div id="right-column">
+        <div id="follow-me">
+            I'm following you!
+        </div>
+    </div>
+
+    <div id="left-column">
+      Content
+    </div>
+</div>
+```
+
+```css
+#container {
+    overflow: auto;
+}
+
+#left-column {
+    width: 70%;
+    float: right;
+}
+
+#right-column {
+    width: 30%;
+    float: right;
+}
+```
+
+```javascript
+  $("#follow-me").follow();
+```
+
+Make sure to include the name of your container if you've named it something other than "container".
+
+```javascript
+  $("#follow-me").follow(
+    container: "my-container-name"
+    );
+```
+... where "my-container-name" is an **element ID** and is the container that **spans the entire height** of the area you want the element to follow you in.
---
+++ jquery.divfollow.js
@@ -0,0 +1,62 @@
+/* jquery.divfollow
+   -- version 1.0
+   -- http://anthonydrakefrost.com
+
+   Feel free to do whatever you'd like with this code.
+
+
+   - Container must be an ID
+*/
+
+(function ($) {
+
+  $.fn.follow = function(options) {
+
+    var settings = $.extend({
+        container: "container",
+    }, options);
+
+    return this.each(function() {
+      var $container = $("#" + settings.container),
+          $mark = $(this),
+          markDistanceFromPageTop = $container.offset().top,
+          markHeight = $mark.outerHeight(),
+          containerHeight = $container.height(),
+          leeway = containerHeight - markHeight;
+
+      calculateScroll($mark, markDistanceFromPageTop, leeway);
+      $(window).scroll(function() { calculateScroll($mark, markDistanceFromPageTop, leeway); });
+    });
+  };
+
+  function calculateScroll($mark, markDistanceFromPageTop, leeway) {
+    // get our current distance from the top
+    var viewportDistanceFromPageTop = $(window).scrollTop();
+
+    // STEP ONE: determine if we've passed the mark
+    if(viewportDistanceFromPageTop > markDistanceFromPageTop) {
+
+      // STEP TWO: determine how much to move the mark
+      var amountToMove = viewportDistanceFromPageTop - markDistanceFromPageTop;
+
+      // STEP THREE: make sure it won't move out of the container
+      if(amountToMove < leeway) {
+        $mark.stop().animate({"marginTop": amountToMove + "px"}, "slow" );
+      }
+
+      // YES, we have touched the bottom
+      else {
+        $mark.stop().animate({"marginTop": leeway + "px"}, "slow" );
+      }
+    }
+
+    // NO, we haven't. Move the mark to the top.
+    else {
+      $mark.stop().animate({"marginTop": "0px"}, "slow" );
+    }
+  }
+}(jQuery));
+
+$(document).ready(function() {
+  $("#follow-me").follow();
+});
